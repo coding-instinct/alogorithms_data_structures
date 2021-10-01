c7bfbccddd9f1a9f963313f1121771074e15069a
Madhuryabarche10 <90447766+Madhuryabarche10@users.noreply.github.com>
2021-10-01 14:48:41 +0530

    codes in c

---
+++ 2.variable.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+/* this is our first c program
+which is awesum
+*/
+
+int main()
+{
+    int a = 4;
+    // int b= 8.5;// this not recommnded because 8.5 is not integer
+    float b = 8.5;
+    char c = 'u'; 
+    int d = 44;
+    int e = 4 ;
+    printf(" the value of a is %d \n" , a);
+    printf("the value of b is %f \n",b);
+    printf("the value of c is %c \n",c);
+    printf("sum of a and d is %d \n",a + d);
+    printf(" SUM  of a and e is %d \n ",a + e);
+ 
+ 
+    return 0;
+
+}
+
