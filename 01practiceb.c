1a7c3f8782ef87aaf56323d6802e38e07ab4caa9
Madhuryabarche10 <90447766+Madhuryabarche10@users.noreply.github.com>
2021-10-01 14:45:01 +0530

    c code

---
+++ 01practiceb.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main (){
+int length , breadth;
+printf("what is the length of reactangle\n");
+scanf("%d",&length);
+
+printf("what is the breadth  of reactangle\n");
+scanf("%d",&breadth);
+  
+  printf("The area of your reactangle is %d ", length*breadth);
+    
+    return 0;
+}
