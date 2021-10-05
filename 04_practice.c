f67423496584a68bf212623186b89cdae30cb4ba
MadhuryaBarche16 <91951960+MadhuryaBarche16@users.noreply.github.com>
2021-10-05 16:32:26 +0530

    04_practice.c

---
+++ 04_practice.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main (){ 
+    int principal=20 , rate= 7.65 ,   years =  7 ;  
+    int simpleIntrest = (principal*rate*years)/100;
+    printf("the value of simpleIntrest is %d", simpleIntrest);
+    return 0;
+} 
+
